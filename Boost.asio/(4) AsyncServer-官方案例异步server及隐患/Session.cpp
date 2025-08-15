c8fdec65b0dd3c3642efa5c7c532976c7c109167
iWang-g <3509932519@qq.com>
2025-08-15 21:51:24 +0800

    feat: 增加发送队列实现全双工通信

--- Boost.asio/(4) AsyncServer-官方案例异步server及隐患/Session.cpp
+++ Boost.asio/(4) AsyncServer-官方案例异步server及隐患/Session.cpp
@@ -4,12 +4,30 @@ void Session::Start()
 {
 	memset(_data, 0, max_length);
 	_socket.async_read_some(boost::asio::buffer(_data, max_length),
-		std::bind(&Session::handle_read, this, std::placeholders::_1, std::placeholders::_2, shared_from_this()));
+		std::bind(&Session::HandleRead, this, std::placeholders::_1, std::placeholders::_2, shared_from_this()));
 }
 
 std::string& Session::GetUuid()
 {
 	return _uuid;
+}
+
+void Session::Send(char* msg, int length)
+{
+	// pending为true表示发送队列里有数据，也就是上一次的数据未发完
+	// pending为false表示上一次的数据发完了，发送缓冲区是空的
+	bool pending = false;
+	std::lock_guard<std::mutex> lock(_send_lock);
+	if (_send_que.size() > 0) {
+		pending = true;
+	}
+	_send_que.push(make_shared<MsgNode>(msg, length));
+	if (pending) {
+		return;
+	}
+
+	boost::asio::async_write(_socket, boost::asio::buffer(msg, length),
+		std::bind(&Session::HandleWrite, this, std::placeholders::_1, shared_from_this()));
 }
 
 void Session::handle_read(const boost::system::error_code& error, std::size_t bytes_transferred, shared_ptr<Session> _self_shared) {
@@ -35,6 +53,38 @@ void Session::handle_write(const boost::system::error_code& error, shared_ptr<Se
 	else {
 		cout << "Write error, error value is " << error.value() << ".Message is " << error.message() << endl;
 		// delete this;
+		_server->ClearSession(_uuid);
+	}
+}
+
+void Session::HandleRead(const boost::system::error_code& error, std::size_t bytes_transferred, shared_ptr<Session> _self_shared)
+{
+	if (!error) {
+		cout << "Server receive data is: " << _data << endl;
+		Send(_data, bytes_transferred);
+		memset(_data, 0, max_length);
+		_socket.async_read_some(boost::asio::buffer(_data, max_length),
+			std::bind(&Session::HandleRead, this, placeholders::_1, placeholders::_2, _self_shared));
+	}
+	else {
+		cout << "Handle read error, error value is " << error.value() << ".Message is " << error.message() << endl;
+		_server->ClearSession(_uuid);
+	}
+}
+
+void Session::HandleWrite(const boost::system::error_code& error, shared_ptr<Session> _self_shared)
+{
+	if (!error) {
+		std::lock_guard<std::mutex> lock(_send_lock);
+		_send_que.pop();
+		if (!_send_que.empty()) {
+			auto& msgnode = _send_que.front();
+			boost::asio::async_write(_socket, boost::asio::buffer(msgnode->_data, msgnode->_max_len),
+				std::bind(&Session::HandleWrite, this, std::placeholders::_1, _self_shared));
+		}
+	}
+	else {
+		cout << "Handle write error, error value is " << error.value() << ".Message is " << error.message() << endl;
 		_server->ClearSession(_uuid);
 	}
 }
--- Boost.asio/(4) AsyncServer-官方案例异步server及隐患/Session.h
+++ Boost.asio/(4) AsyncServer-官方案例异步server及隐患/Session.h
@@ -4,10 +4,31 @@
 #include <map>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <queue>
+#include <memory>
 using boost::asio::ip::tcp;
 using namespace std;
 
 class Server;
+
+class MsgNode {
+	friend class Session;
+public:
+	MsgNode(char* msg, int max_len) {
+		_data = new char[max_len];
+		memcpy(_data, msg, max_len);
+	}
+	
+	~MsgNode() {
+		delete[] _data;
+	}
+
+private:
+	int _cur;
+	int _max_len;
+	char* _data;
+};
+
 class Session : public std::enable_shared_from_this<Session>
 {
 public:
@@ -26,16 +47,21 @@ public:
 
 	void Start();
 	std::string& GetUuid();
+	void Send(char* msg, int length);
 
 private:
 	void handle_read(const boost::system::error_code& error, std::size_t bytes_transferred, shared_ptr<Session> _self_shared);
 	void handle_write(const boost::system::error_code& error, shared_ptr<Session> _self_shared);
+	void HandleRead(const boost::system::error_code& error, std::size_t bytes_transferred, shared_ptr<Session> _self_shared);
+	void HandleWrite(const boost::system::error_code& error, shared_ptr<Session> _self_shared);
 
 	tcp::socket _socket;
 	enum { max_length = 1024 };
 	char _data[max_length];
 	Server* _server;
 	std::string _uuid;
+	std::queue<shared_ptr<MsgNode>> _send_que;
+	std::mutex _send_lock;
 };
 
 class Server {
