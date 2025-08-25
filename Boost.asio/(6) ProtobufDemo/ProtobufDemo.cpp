e4b608a476111bb7f2b7327a3b53f3793b124f90
iWang-g <3509932519@qq.com>
2025-08-25 22:00:29 +0800

    feat: 添加ProtobufDemo

---
+++ Boost.asio/(6) ProtobufDemo/ProtobufDemo.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "msg.pb.h"
+
+int main()
+{
+    Book book;
+    book.set_name("CPP programing");
+    book.set_pages(100);
+    book.set_price(200);
+    std::string bookstr;
+    book.SerializeToString(&bookstr);
+    std::cout << "Serialize str is " << bookstr << std::endl;
+    Book book2;
+    book2.ParseFromString(bookstr);
+    std::cout << "book2 name is " << book2.name() << ",pages is " << book2.pages()
+        << ",price is " << book2.price() << std::endl;
+    getchar();
+}
